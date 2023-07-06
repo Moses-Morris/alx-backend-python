7a21b60cadfb5c31ce706edf8a5435b9c5fce3bf
Moses-Morris <mosesmorrisdev@gmail.com>
2023-07-06 11:55:19 +0000

    Create a tuple.

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Optional and Use a tuple"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Type anotate the return elements of a tuple"""
+    return (k, v * v)
