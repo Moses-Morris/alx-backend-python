17309c6da924b1376973404c7d073802d8061d0f
Moses-Morris <mosesmorrisdev@gmail.com>
2023-07-06 11:39:28 +0000

    Return a List from the imported List module.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""List tupe annotation arguments"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sum the values of the list"""
+    return  sum(input_list)
