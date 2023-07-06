a70ffc8a49fe758c657108e8742d74cf86513a84
Moses-Morris <mosesmorrisdev@gmail.com>
2023-07-06 11:48:01 +0000

    Use of Union to type annotate many types of a variable.

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""The best Union Tune"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return the sum as a float"""
+    return sum(mxd_lst)
