a056a15616b7520ce0c62d9032b15b3e0a4f3c3c
Moses-Morris <mosesmorrisdev@gmail.com>
2023-07-10 13:40:01 +0000

    Arouse

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-''' async and await syntax '''
 import asyncio
 from typing import List
-
-get = __import__('3-tasks').task_wait_random
+from heapq import nsmallest
+task_wait_random = __import__('3-tasks').task_wait_random
+"""uses task_wait_random"""
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    ''' Function that returns a list
-    '''
-    l = [get(max_delay) for i in range(n)]
-    finish = [await task for task in asyncio.as_completed(l)]
-    return finish
+    """uses task_wait_random"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return nsmallest(n, results)
