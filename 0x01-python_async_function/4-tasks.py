ad9660233fb6ef9923454cadc96e35bd4f4d245e
Moses-Morris <mosesmorrisdev@gmail.com>
2023-07-10 13:52:49 +0000

    Create a random task : fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
+'''new function task_wait_n
+'''
 import asyncio
 from typing import List
-from heapq import nsmallest
+
+
 task_wait_random = __import__('3-tasks').task_wait_random
-"""uses task_wait_random"""
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """uses task_wait_random"""
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*tasks)
-    return nsmallest(n, results)
+    '''Runs task_wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
